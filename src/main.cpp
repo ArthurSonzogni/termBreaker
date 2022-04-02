fecc0a75ce138473365316d9fa4be3db9d9452ba
Template Janitor <template.janitor@example.com>
2022-04-02 08:10:56 +0000

    Cleanup template and initialize repository

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include <spdlog/spdlog.h>
 
 // This file will be generated automatically when you run the CMake
-// configuration step. It creates a namespace called `myproject`. You can modify
+// configuration step. It creates a namespace called `termRider`. You can modify
 // the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
@@ -322,8 +322,8 @@ int main(int argc, const char **argv)
       { std::next(argv), std::next(argv, argc) },
       true,// show help if requested
       fmt::format("{} {}",
-        myproject::cmake::project_name,
-        myproject::cmake::project_version));// version string, acquired
+        termRider::cmake::project_name,
+        termRider::cmake::project_version));// version string, acquired
                                             // from config.hpp via CMake
 
     if (args["turn_based"].asBool()) {
